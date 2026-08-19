f508dbb75bb871227b45fc893dec94d2058cf9f3
sehrishbibi01-CS <sehrish.biibi01@gmail.com>
2026-08-19 16:40:09 +0500

    tailwind

---
+++ tailwind/tailwind/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    
+<div className="sm:bg-amber-500  md:bg-amber-700">
+      <p class= "text-pink"> i appear on screen wider than 768px
+
+      </p>
+    </div>
+  )
+}
+
+export default App
